9836541db499dfb942a5818bcbef64d21f622415
TerminusBot <7903661+TerminusBot@users.noreply.github.com>
2018-05-10 14:10:01 +0800

    Create ArchiveTeleBot.py

---
+++ ArchiveTeleBot.py
@@ -0,0 +1,10 @@
+import telebot
+import archiveis
+
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(regexp="(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])")
+def echo_all(message):
+	bot.reply_to(message, archiveis.capture(message.text))
+
+bot.polling()
