7d72517ee9439add9da5960d690467dd96830a72
TerminusBot <>
2018-09-09 10:59:23 +0800

    HATE PYTHON

--- ArchiveTeleBot.py
+++ ArchiveTeleBot.py
@@ -11,22 +11,23 @@ bot = telebot.TeleBot("TOKEN", threaded=False)
 def echo_all(message):
 	reply = archiveis.capture(message.text)
 	bot.reply_to(message, reply)
+	html = requests.get(message.text)
+	Title = BeautifulSoup(html.text, "html.parser").title.text.encode('utf-8').strip()
 
-        html = requests.get(message.text)
-        Soup = BeautifulSoup(html.text, "html.parser")
+	with open('archive.csv', 'a') as f1:
+		f1.write(time.ctime() + ',' + message.text + ',' + reply + ',')
+		f1.write(Title)
+		f1.write('\n')
 
-	with open('archive.txt', 'a') as f:
-		f.write(time.ctime() + '\n' + message.text + '\n' + reply)
-        with open('archive.txt', 'a') as f:
-            f.write('\n' + '\n')
-            with open('archive.txt', 'a') as f:
-                f.write(Soup.title.text.encode('utf-8'))
-
+	with open('archive.txt', 'a') as f2:
+		f2.write(time.ctime() + '\n' + message.text + '\n' + reply + '\n')
+		f2.write(Title)
+		f2.write('\n' + '\n')
 
 while True:
     try:
         bot.polling(none_stop=True, timeout=123)
     except Exception as e:
-	logger = telebot.logger
-        logger.error(e)
-        time.sleep(15)
+		logger = telebot.logger
+        	logger.error(e)
+        	time.sleep(15)
