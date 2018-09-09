b3e768b52d38148f441201ce2cd56d4cac615bc9
TerminusBot <>
2018-09-09 22:20:09 +0800

    BETTER FORMAT

--- data/check.R
+++ data/check.R
@@ -8,7 +8,7 @@ createLink <- function(link, text) {
   sprintf(l, link)
 }
 
-# Define UI for data download app ----
+
 ui <- function(input, output, session){
 
   navbarPage(
@@ -20,19 +20,21 @@ ui <- function(input, output, session){
 }
 
 
-# Define server logic to display and download selected file ----
 server <- function(input, output, session) {
 
   archive2 = reactiveFileReader(60000, session, 'archive2.csv', read_csv)
   output$table1 <- DT::renderDataTable({
 
     archive2 = archive2()
-    archive2$V2 = createLink(archive2$V2, archive2$V4)
-    archive2$V3 = createLink(archive2$V3, archive2$V3)
+    names(archive2) = c('Time', 'Title_url', 'Archive_url', 'Title', 'Check')
+    archive2$Time = substr(archive2$Time, 5, nchar(archive2$Time))
+    archive2$Time = parse_datetime(archive2$Time, format = "%b %d %H:%M:%S %Y")
+    archive2$Time = archive2$Time + 8*60*60
+    archive2$Title_url = createLink(archive2$Title_url, archive2$Title)
+    archive2$Archive_url = createLink(archive2$Archive_url, archive2$Archive_url)
     DT::datatable(archive2[, c(1,2,3,5)], escape = FALSE)
   })
 
 }
 
-# Create Shiny app ----
 shinyApp(ui, server)
