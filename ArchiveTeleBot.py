6f75f875d8b8d2a1bd5b8a99e0b2818b4548a03a
TerminusBot <terminus2049@protonmail.com>
2019-05-01 11:02:22 +0800

    对微博网址进行提示

--- ArchiveTeleBot.py
+++ ArchiveTeleBot.py
@@ -9,20 +9,25 @@ bot = telebot.TeleBot("TOKEN", threaded=False)
 
 @bot.message_handler(regexp="(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])")
 def echo_all(message):
-	reply = archivenow.push(message.text, 'is')[0]
-	try:
-		bot.reply_to(message, reply)
-	except Exception as e:
-		bot.reply_to(message, 'oooops, please send the url again.')
+	    if 'weibo.com' in message.text or 'weibo.cn' in message.text:
+        try:
+            bot.reply_to(message, "archive doesn't support weibo")
+        except Exception as e:
+            bot.reply_to(message, 'oooops, please send the url again.')
+    else:
+        try:
+            bot.reply_to(message, reply)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, please send the url again.')
 
-	html = requests.get(message.text)
-	Title = BeautifulSoup(html.text, "html.parser").title.text.encode('utf-8').strip()
+        html = requests.get(message.text)
+        Title = BeautifulSoup(html.text, "html.parser").title.text.encode('utf-8').strip()
 
-	with open('archive.csv', 'a') as f1:
-		f1.write(time.ctime() + ',' + message.text + ',' + reply + ',')
-		f1.write(Title)
-		f1.write('\n')
-
+        with open('archive.csv', 'a') as f1:
+            f1.write(time.ctime() + ',' + message.text + ',' + reply + ',')
+            f1.write(Title)
+            f1.write('\n')
+	
 while True:
     try:
         bot.polling(none_stop=True, timeout=123)
