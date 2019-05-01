05de4c38087e83b273f99a47bef86ccc2fb33dd5
TerminusBot <terminus2049@protonmail.com>
2019-05-01 19:22:29 +0800

    Update ArchiveTeleBot.py

--- ArchiveTeleBot.py
+++ ArchiveTeleBot.py
@@ -1,5 +1,4 @@
 import telebot
-import logging
 import time
 import requests
 from archivenow import archivenow
@@ -7,9 +6,12 @@ from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot("TOKEN", threaded=False)
 
-@bot.message_handler(regexp="(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])")
+
+@bot.message_handler(
+    regexp="(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])")
 def echo_all(message):
-	    if 'weibo.com' in message.text or 'weibo.cn' in message.text:
+    reply = archivenow.push(message.text, 'is')[0]
+    if 'weibo.com' in message.text or 'weibo.cn' in message.text:
         try:
             bot.reply_to(message, "archive doesn't support weibo")
         except Exception as e:
@@ -27,11 +29,12 @@ def echo_all(message):
             f1.write(time.ctime() + ',' + message.text + ',' + reply + ',')
             f1.write(Title)
             f1.write('\n')
-	
+
+
 while True:
     try:
         bot.polling(none_stop=True, timeout=123)
     except Exception as e:
-		logger = telebot.logger
-        	logger.error(e)
-        	time.sleep(15)
+        logger = telebot.logger
+        logger.error(e)
+        time.sleep(15)
