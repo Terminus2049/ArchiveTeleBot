1fc3c7211f1f960c13ce9f5bd5b9ec7abb01c364
TerminusBot <terminus2049@protonmail.com>
2020-01-11 05:26:51 +0000

    移除archive.is，采用自建存档服务

--- ArchiveTeleBot.py
+++ ArchiveTeleBot.py
@@ -1,16 +1,16 @@
 import telebot
 import time
 import requests
+import os
 from archivenow import archivenow
 from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot("TOKEN", threaded=False)
 
-
 @bot.message_handler(
     regexp="(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])")
 def echo_all(message):
-    
+
     if 'weibo.com' in message.text or 'weibo.cn' in message.text:
         try:
             bot.reply_to(message, "archive doesn't support weibo")
@@ -21,9 +21,8 @@ def echo_all(message):
             url = '&'.join(message.text.split('&', 5)[:5])
         else:
             url = message.text
+
         try:
-            reply_is = archivenow.push(url, 'is')[0]
-            bot.reply_to(message, reply_is)
             reply_ia = archivenow.push(url, 'ia')[0]
             bot.reply_to(message, reply_ia)
         except Exception as e:
@@ -34,16 +33,23 @@ def echo_all(message):
         if message.text.startswith('https://mp.weixin.qq.com/s'):
             Title = soup.h2.text.strip()
             Title = Title.replace(',',' ')
+            Title = Title.replace(' ','_')
         else:
             Title = soup.title.text.strip()
             Title = Title.replace('\n','')
             Title = Title.replace(',',' ')
+            Title = Title.replace(' ','_')
 
+        cmd = 'monolith ' + url + ' -o /srv/web/mono/' + Title + '.html'
+        print(cmd)
+        os.system(cmd)
+        reply_url = 'http://206.189.252.32:8083/'  + Title + '.html'
+        bot.reply_to(message, reply_url)
         with open('archive.csv', 'a') as f1:
-            f1.write(time.ctime() + ',' + message.text + ',' + reply_is + ',')
+            print(reply_ia)
+            f1.write(time.ctime() + ',' + message.text + ',' + reply_ia + ',')
             f1.write(Title)
             f1.write('\n')
-
 
 while True:
     try:
