f9276d27b754af837837098bbd5973299a9baca6
TerminusBot <terminus2049@protonmail.com>
2020-01-12 17:37:19 +0000

    Update ArchiveTeleBot.py

--- ArchiveTeleBot.py
+++ ArchiveTeleBot.py
@@ -1,9 +1,11 @@
+import datetime
 import telebot
 import time
 import requests
-import os
 from archivenow import archivenow
 from bs4 import BeautifulSoup
+import os
+import random
 
 bot = telebot.TeleBot("TOKEN", threaded=False)
 
@@ -22,29 +24,44 @@ def echo_all(message):
         else:
             url = message.text
 
-        try:
-            reply_ia = archivenow.push(url, 'ia')[0]
-            bot.reply_to(message, reply_ia)
-        except Exception as e:
-            bot.reply_to(message, 'oooops, please send the url again.')
-
         html = requests.get(url)
         soup = BeautifulSoup(html.text, "html.parser")
-        if message.text.startswith('https://mp.weixin.qq.com/s'):
-            Title = soup.h2.text.strip()
-            Title = Title.replace(',',' ')
-            Title = Title.replace(' ','_')
+        if len(soup) == 0:
+            Title = datetime.datetime.now().strftime("%Y-%m-%d-") + str(random.randrange(2, 50000000))
         else:
-            Title = soup.title.text.strip()
-            Title = Title.replace('\n','')
-            Title = Title.replace(',',' ')
-            Title = Title.replace(' ','_')
+            if message.text.startswith('https://mp.weixin.qq.com/s'):
+                Title = soup.h2.text.strip()
+                Title = Title.replace('|',' ')
+                Title = Title.replace('<',' ')
+                Title = Title.replace('>',' ')
+                Title = Title.replace(',',' ')
+                Title = Title.replace(' ','_')
+            else:
+                Title = soup.title.text.strip()
+                Title = Title.replace('|',' ')
+                Title = Title.replace('<',' ')
+                Title = Title.replace('>',' ')
+                Title = Title.replace('\n','')
 
         cmd = 'monolith ' + url + ' -o /srv/web/mono/' + Title + '.html'
         print(cmd)
         os.system(cmd)
         reply_url = 'http://206.189.252.32:8083/'  + Title + '.html'
         bot.reply_to(message, reply_url)
+
+
+        try:
+            reply_ia = archivenow.push(url, 'ia')[0]
+            bot.reply_to(message, reply_ia)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, please send the url again.')
+            logger = telebot.logger
+            logger.error(e)
+            time.sleep(15)
+
+
+
+
         with open('archive.csv', 'a') as f1:
             print(reply_ia)
             f1.write(time.ctime() + ',' + message.text + ',' + reply_ia + ',')
@@ -54,7 +71,4 @@ def echo_all(message):
 while True:
     try:
         bot.polling(none_stop=True, timeout=123)
-    except Exception as e:
-        logger = telebot.logger
-        logger.error(e)
-        time.sleep(15)
+                
